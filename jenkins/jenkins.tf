52bf4a88f3aadc7774a75137f5116f365426f8f2
morleystuff <morleyj.mail@gmail.com>
2021-05-06 09:15:54 +0200

    Updates for jenkins definition

--- jenkins/jenkins.tf
+++ jenkins/jenkins.tf
@@ -1,17 +1,26 @@
-resource "aws_spot_instance_request" "JenkinsMaster" {
+resource "aws_spot_instance_request" "jenkins_master" {
 
-    spot_price = "0.005"
-
-    ami = "ami-0e1a69c81175d6bf2"
-    instance_type = "t2.micro"
-    subnet_id = aws_subnet.subnet1.id
-    security_groups = [
-        aws_security_group.SecurityGroupSSH.id,
-        aws_security_group.SecurityGroupWebServer.id
+    // Machine definition
+    instance_type        = "t2.micro"
+    ami                  = "ami-0e1a69c81175d6bf2"
+    subnet_id            = aws_subnet.subnet1.id
+    
+    // Spot pricing
+    spot_price           = "0.005"
+    wait_for_fulfillment = "true"
+    
+    // Security
+    key_name             = "ssh-laptop"
+    security_groups      = [
+        aws_security_group.sec_group_ssh.id,
+        aws_security_group.sec_group_web.id
     ]
-    wait_for_fulfillment = true
-    key_name = "ssh-laptop"
 
+    tags = {
+      "Name" = "JenkinsMaster"
+    }
+
+    // Initialization
     user_data = <<EOF
 #!/bin/bash
 
@@ -31,41 +40,92 @@ EOF
 
 }
 
+resource "aws_spot_instance_request" "jenkins_linux_worker" {
+
+    // Machine definition
+    instance_type        = "t2.micro"
+    ami                  = "ami-0e1a69c81175d6bf2"
+    subnet_id            = aws_subnet.subnet1.id
+    
+    // Spot pricing
+    spot_price           = "0.005"
+    wait_for_fulfillment = "true"
+    
+    // Security
+    key_name             = "ssh-laptop"
+    security_groups      = [
+        aws_security_group.sec_group_ssh.id,
+        aws_security_group.sec_group_web.id
+    ]
+
+    tags = {
+      "Name" = "LinuxWorker"
+    }
+
+}
+
+resource "aws_spot_instance_request" "jenkins_windows_worker" {
+
+    // Machine definition
+    instance_type        = "t2.micro"
+    ami                  = "ami-08d971022fd230af2"
+    subnet_id            = aws_subnet.subnet1.id
+    
+    // Spot pricing
+    spot_price           = "0.01"
+    wait_for_fulfillment = "true"
+    
+    // Security
+    key_name             = "ssh-laptop"
+    security_groups      = [
+        aws_security_group.sec_group_ssh.id,
+        aws_security_group.sec_group_web.id
+    ]
+
+    tags = {
+      "Name" = "WindowsWorker"
+    }
+
+}
+
 resource "aws_lb" "load_balancer" {
-    name = "JenkinsLoadBalancer"
+    name               = "JenkinsLoadBalancer"
     load_balancer_type = "application"
-    security_groups = [ aws_security_group.SecurityGroupWebServer.id ]
-    subnets = [ aws_subnet.subnet1.id, aws_subnet.subnet2.id ]
+    security_groups    = [ aws_security_group.sec_group_web.id ]
+    subnets            = [ 
+        aws_subnet.subnet1.id,
+        aws_subnet.subnet2.id
+    ]
 }
 
 resource "aws_lb_target_group_attachment" "Target" {
     target_group_arn = aws_lb_target_group.target_group.id
-    port = 80
-    target_id = "${aws_spot_instance_request.JenkinsMaster.private_ip}"
+    port             = "80"
+    target_id        = aws_spot_instance_request.jenkins_master.private_ip
 }
 
 resource "aws_lb_target_group" "target_group" {
-    name = "JenkinsTargetGroup"
+    name        = "JenkinsTargetGroup"
     target_type = "ip"
-    port = 80
-    protocol = "HTTP"
-    vpc_id = aws_vpc.morleystuff_vpc.id
+    port        = "80"
+    protocol    = "HTTP"
+    vpc_id      = aws_vpc.vpc.id
     health_check {
-      path = "/login"
+      path      = "/login"
     }
 }
 
 resource "aws_lb_listener" "http_listener" {
     load_balancer_arn = aws_lb.load_balancer.id
-    port = 80 
-    protocol = "HTTP"
+    port              = "80" 
+    protocol          = "HTTP"
     default_action {
         type = "redirect"
         redirect {
-            host = "#{host}"
-            path = "/#{path}"
-            port = "443"
-            protocol = "HTTPS"
+            host        = "#{host}"
+            path        = "/#{path}"
+            port        = "443"
+            protocol    = "HTTPS"
             status_code = "HTTP_301"
         }
     }
@@ -73,9 +133,9 @@ resource "aws_lb_listener" "http_listener" {
 
 resource "aws_lb_listener" "https_listener" {
     load_balancer_arn = aws_lb.load_balancer.id
-    port = 443
-    protocol = "HTTPS"
-    certificate_arn = "arn:aws:acm:ap-southeast-2:365033114011:certificate/cb73021b-6107-47ec-8f0e-dc02dcb6ec25"
+    port              = "443"
+    protocol          = "HTTPS"
+    certificate_arn   = "arn:aws:acm:ap-southeast-2:365033114011:certificate/cb73021b-6107-47ec-8f0e-dc02dcb6ec25"
     default_action {
         type = "forward"
         target_group_arn = aws_lb_target_group.target_group.id
@@ -84,11 +144,11 @@ resource "aws_lb_listener" "https_listener" {
 
 resource "aws_route53_record" "a_record" {
     zone_id = "ZT0O09PTBTJOV"
-    name = "jenkins.morleystuff.com"
-    type = "A"
+    name    = "jenkins.morleystuff.com"
+    type    = "A"
     alias {
-        zone_id = aws_lb.load_balancer.zone_id
-        name = aws_lb.load_balancer.dns_name
-        evaluate_target_health = false
+        zone_id                = aws_lb.load_balancer.zone_id
+        name                   = aws_lb.load_balancer.dns_name
+        evaluate_target_health = "false"
     }
 }
--- jenkins/network.tf
+++ jenkins/network.tf
@@ -3,92 +3,97 @@ provider "aws" {
     region  = "ap-southeast-2"
 }
 
-resource "aws_vpc" "morleystuff_vpc" {
+resource "aws_vpc" "vpc" {
     cidr_block = "10.0.0.0/24"
 }
 
-resource "aws_internet_gateway" "internet_gateway" {
-    vpc_id = aws_vpc.morleystuff_vpc.id
+resource "aws_internet_gateway" "inet_gateway" {
+    vpc_id = aws_vpc.vpc.id
 }
 
 resource "aws_route_table" "route" {
-    vpc_id = aws_vpc.morleystuff_vpc.id
+    vpc_id = aws_vpc.vpc.id
 
     route {
-        gateway_id = aws_internet_gateway.internet_gateway.id
+        gateway_id = aws_internet_gateway.inet_gateway.id
         cidr_block = "0.0.0.0/0"
     }
 }
 
 resource "aws_network_acl" "nacl" {
-    vpc_id = aws_vpc.morleystuff_vpc.id
+    vpc_id = aws_vpc.vpc.id
+
     subnet_ids = [
         aws_subnet.subnet1.id,
         aws_subnet.subnet2.id
     ]
 
     egress {
-        action = "allow"
-        rule_no = 100
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
+        action     = "allow"
+        rule_no    = "100"
+        from_port  = "0"
+        to_port    = "0"
+        protocol   = "-1"
         cidr_block = "0.0.0.0/0"
     }
 
     ingress {
-        action = "allow"
+        action     = "allow"
         cidr_block = "0.0.0.0/0"
-        from_port = 22
-        protocol = "tcp"
-        rule_no = 100
-        to_port = 22
+        from_port  = "22"
+        protocol   = "tcp"
+        rule_no    = "100"
+        to_port    = "22"
     }
     ingress {
-        action = "allow"
+        action     = "allow"
         cidr_block = "0.0.0.0/0"
-        from_port = 80
-        protocol = "tcp"
-        rule_no = 101
-        to_port = 80
+        from_port  = "80"
+        protocol   = "tcp"
+        rule_no    = "101"
+        to_port    = "80"
     }
     ingress {
-        action = "allow"
+        action     = "allow"
         cidr_block = "0.0.0.0/0"
-        from_port = 443
-        protocol = "tcp"
-        rule_no = 102
-        to_port = 443
+        from_port  = "443"
+        protocol   = "tcp"
+        rule_no    = "102"
+        to_port    = "443"
     }
     ingress {
-        action = "allow"
+        action     = "allow"
         cidr_block = "0.0.0.0/0"
-        from_port = 1024
-        protocol = "tcp"
-        rule_no = 103
-        to_port = 65535
+        from_port  = "1024"
+        protocol   = "tcp"
+        rule_no    = "103"
+        to_port    = "65535"
     }
     
 }
 
 resource "aws_subnet" "subnet1" {
-    vpc_id = aws_vpc.morleystuff_vpc.id
-    cidr_block = "10.0.0.0/26"
-    map_public_ip_on_launch = true
-}
-
-resource "aws_route_table_association" "subnet1_route" {
-  subnet_id = aws_subnet.subnet1.id
-  route_table_id = aws_route_table.route.id
+    vpc_id                  = aws_vpc.vpc.id
+    cidr_block              = "10.0.0.0/26"
+    availability_zone_id    = "apse2-az1"
+    map_public_ip_on_launch = "true"
 }
 
 resource "aws_subnet" "subnet2" {
-  vpc_id = aws_vpc.morleystuff_vpc.id
-  cidr_block = "10.0.0.128/26"
-  map_public_ip_on_launch = true
+    vpc_id                  = aws_vpc.vpc.id
+    cidr_block              = "10.0.0.128/26"
+    availability_zone_id    = "apse2-az3"
+    map_public_ip_on_launch = "true"
 }
 
+resource "aws_route_table_association" "subnet1_route" {
+    subnet_id      = aws_subnet.subnet1.id
+    route_table_id = aws_route_table.route.id
+}
+
+
+
 resource "aws_route_table_association" "subnet2_route" {
-  subnet_id = aws_subnet.subnet2.id
-  route_table_id = aws_route_table.route.id
+    subnet_id      = aws_subnet.subnet2.id
+    route_table_id = aws_route_table.route.id
 }
