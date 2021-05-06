b6433056f21adc59f29cb989d35e3d684e994259
morleystuff <morleyj.mail@gmail.com>
2021-05-06 09:15:30 +0200

    Added definition for valheim server

---
+++ valheim/network.tf
@@ -0,0 +1,100 @@
+resource "aws_vpc" "vpc" {
+    cidr_block = "10.0.0.0/24"
+}
+
+resource "aws_internet_gateway" "inet_gateway" {
+    vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_route_table" "route" {
+    vpc_id = aws_vpc.vpc.id
+
+    route {
+        gateway_id = aws_internet_gateway.inet_gateway.id
+        cidr_block = "0.0.0.0/0"
+    }
+}
+
+resource "aws_network_acl" "nacl" {
+    vpc_id = aws_vpc.vpc.id
+
+    subnet_ids = [
+        aws_subnet.subnet1.id,
+        aws_subnet.subnet2.id
+    ]
+
+    egress {
+        action     = "allow"
+        rule_no    = "100"
+        from_port  = "0"
+        to_port    = "0"
+        protocol   = "-1"
+        cidr_block = "0.0.0.0/0"
+    }
+
+    ingress {
+        action     = "allow"
+        cidr_block = "0.0.0.0/0"
+        from_port  = "22"
+        protocol   = "tcp"
+        rule_no    = "100"
+        to_port    = "22"
+    }
+    ingress {
+        action     = "allow"
+        cidr_block = "0.0.0.0/0"
+        from_port  = "80"
+        protocol   = "tcp"
+        rule_no    = "101"
+        to_port    = "80"
+    }
+    ingress {
+        action     = "allow"
+        cidr_block = "0.0.0.0/0"
+        from_port  = "443"
+        protocol   = "tcp"
+        rule_no    = "102"
+        to_port    = "443"
+    }
+    ingress {
+        action     = "allow"
+        cidr_block = "0.0.0.0/0"
+        from_port  = "1024"
+        protocol   = "tcp"
+        rule_no    = "103"
+        to_port    = "65535"
+    }
+    ingress {
+        action     = "allow"
+        cidr_block = "0.0.0.0/0"
+        from_port  = "2456"
+        protocol   = "udp"
+        rule_no    = "104"
+        to_port    = "2456"
+    }
+    
+}
+
+resource "aws_subnet" "subnet1" {
+    vpc_id                  = aws_vpc.vpc.id
+    cidr_block              = "10.0.0.0/26"
+    map_public_ip_on_launch = "true"
+}
+
+resource "aws_subnet" "subnet2" {
+    vpc_id                  = aws_vpc.vpc.id
+    cidr_block              = "10.0.0.128/26"
+    map_public_ip_on_launch = "true"
+}
+
+resource "aws_route_table_association" "subnet1_route" {
+    subnet_id      = aws_subnet.subnet1.id
+    route_table_id = aws_route_table.route.id
+}
+
+
+
+resource "aws_route_table_association" "subnet2_route" {
+    subnet_id      = aws_subnet.subnet2.id
+    route_table_id = aws_route_table.route.id
+}
---
+++ valheim/security-groups.tf
@@ -0,0 +1,60 @@
+resource "aws_security_group" "sec_group_ssh" {
+    description = "Allows for inbound traffic on SSH port from any ip"
+    vpc_id      = aws_vpc.vpc.id
+    egress {
+        from_port   = "0"
+        to_port     = "0"
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        protocol    = "tcp"
+        from_port   = "22"
+        to_port     = "22"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    lifecycle {
+        create_before_destroy = true
+    }
+}
+
+resource "aws_security_group" "sec_group_web" {
+    description = "Allows for inbound traffic on HTTP & HTTPS ports"
+    vpc_id      = aws_vpc.vpc.id
+    egress {
+        from_port   = "0"
+        to_port     = "0"
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        protocol    = "tcp"
+        from_port   = "80"
+        to_port     = "80"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        protocol    = "tcp"
+        from_port   = "443"
+        to_port     = "443"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    lifecycle {
+        create_before_destroy = true
+    }
+    
+}
+
+resource "aws_security_group" "sec_group_valheim" {
+    description = "Allows for traffic over valheim server port"
+    vpc_id      = aws_vpc.vpc.id
+    ingress {
+        protocol    = "udp"
+        from_port   = "2456"
+        to_port     = "2456"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    lifecycle {
+        create_before_destroy = true
+    }
+}
